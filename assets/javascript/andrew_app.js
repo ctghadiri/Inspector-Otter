9250a7662e9743fa3bb9cd436a98189da4678b4c
Andrew <xie.andrew2235@gmail.com>
2019-03-12 21:33:19 -0700

    added inline comments for clarification

--- assets/javascript/andrew_app.js
+++ assets/javascript/andrew_app.js
@@ -1,42 +1,44 @@
-// {/* // Initialize Firebase */ }
-// var config = {
-//   apiKey: "AIzaSyD-HAGjErg3mdQnQq8VWDHbTnN85ynAilo",
-//   authDomain: "projectone-51896.firebaseapp.com",
-//   databaseURL: "https://projectone-51896.firebaseio.com",
-//   projectId: "projectone-51896",
-//   storageBucket: "projectone-51896.appspot.com",
-//   messagingSenderId: "1060702068061"
-// };
-// firebase.initializeApp(config);
 
-var database = firebase.database();
+$(".urbanRow").empty();
+// Takes in user input with ID tag #theWord from HTML
+var wordSearch = $("#theWord").val().trim();
 
-var word = $('<section>', { class: 'word' });
+var urbanDictionaryAppend = "<button class='btn btn-primary' id='noFilter' type='button' data-toggle='collapse' data-target='#collapseExample' aria-expanded='false' aria-controls='collapseExample'>Unofficial Definition</button><br><div class='collapse' id='collapseExample'></div><br><hr><br>";
+$("#urbanBody").append(urbanDictionaryAppend);
 
-var searchButton = $("<button>").text("Search").addClass("searchButton")
+// Defines Urban Dictionary URL, which in this case is just the API Link + user input, which we have defined above as "wordSearch"
+var urbanDictionaryQuery = 'http://api.urbandictionary.com/v0/define?term={' + wordSearch + '}'
+// AJAX Call and for loop
+$.ajax({
+  url: urbanDictionaryQuery,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+  // Clears out the class "urbanRow", which was added onto a table row for the definition
+  $(".urbanRow").empty();
+  // Clears out the class "emojiRow", which was added onto a table row for the emoji and # of "thumbs up" that each definition has received on UrbanDictionary website
+  $(".emojiRow").empty();
+  // For loop, which defines the definition and how it will be showed on the page. In order to draw the definition, we go down the list of "response", then "list", and definition
+  for (i = 0; i < 1; i++) {
+    var wordDefinition = 'Unofficial Definition: ' + response.list[i].definition;
+    // Goes down API info list (response.list.thumbs_up) to obtain the # of times the user input term has been given a thumbs up on Urban Dictionary
+    var thumbsUp = ' : ' + response.list[i].thumbs_up;
+    console.log(thumbsUp);
+    // Creating a table row for the definition and for the thumbs up emoji + thumbs up count
+    // The two rows will append under ID "#collapseExample", which is the "unofficial definition" button on the page
+    var tableRowOne = $("<tr>").addClass("urbanRow");
+    var tableRowTwo = $("<tr>").addClass("emojiRow");
+    // Table data; first one displays the variable wordDefinition in text
+    var tableDataOne = $("<td>").text(wordDefinition);
+    // Displays emoji and thumbsUp variable
+    var tableDataTwo = $("<td>").html("👍" + thumbsUp);
 
-$("#submitButton").on("click", function UrbanDictionary() {
-$("tbody").text("")
-var wordSearch = $("#theWord").val();
-word.append(wordSearch);
-$("tbody").append(word);
-$("search").val("");
+    console.log(tableDataTwo);
+    // The first row appends the data from var tableDataOne, and RowTwo appends data from var tableDataTwo
+    tableRowOne.append(tableDataOne);
+    tableRowTwo.append(tableDataTwo);
+    // tableRowOne and tableRowTwo append after the "unofficial definition" button
+    $("#collapseExample").append(tableRowOne, tableRowTwo);
+  }
+});
 
-  var urbanDictionaryQuery = 'http://api.urbandictionary.com/v0/define?term=' + wordSearch;
-  $.ajax({
-    url: urbanDictionaryQuery,
-    method: "GET",
-    // dataType: 'json',
-  }).then(function (response) {
-    console.log(response);
-
-    
-    for (i = 0; i < 5; i++) {
-      var wordDefinition = $('<section class="containerWordDefinition row"><p class="col-12">Definition:' + response.list[i].definition + '</p></section>');
-      // var definition = list.definition;
-    // wordDefinition.append("<row>");
-    // wordDefinition.text(response.definition);
-    $("tbody").append(wordDefinition);
-    $("#EnterWord").val("")
-  }});
-})
